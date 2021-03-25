1db4654d0c02e015671495b5bca7ff0856ffc0d3
Scott Campit <scampit@umich.edu>
2021-03-24 22:36:58 -0400

    Added batch scripts

--- srv/ihuman_scCOBRA.m
+++ srv/ihuman_scCOBRA.m
@@ -4,24 +4,14 @@
 % This script computes the reaction knockouts and metabolic fluxes for the scCOBRA 
 % models.
 %% 1. Load Data
-% First, let's load the COBRA toolbox.
+% A. Load the COBRA Toolbox
 
 % Initialize metabolic modeling components
 clear all;
-initCobraToolbox(false); changeCobraSolver('gurobi', 'all');
-% Load iHUMAN
-% This loads the iHUMAN metabolic reconstruction.
-
-load D:/Data/Mappings/Reconstructions/Human1/ihuman_model_data_dec2020.mat ihuman_hams
-model = ihuman_hams;
-%% 
-% Set the objective function to maximize biomass
-
-% Set up objective function
-model.c = zeros(size(model.c));
-model.c(string(model.rxns) == 'biomass_human') = 1;
-%% 2. Load gene expression dataset
-% First, we'll load the A549 MAGIC-imputed gene expression dataset. 
+initCobraToolbox(false); changeCobraSolver('cplex', 'all');
+% B. Load iHUMAN reconstruction and gene expression dataset
+% First, we'll load the A549 MAGIC-imputed gene expression dataset and the iHUMAN 
+% metabolic reconstruction. 
 % 
 % For more information, see the |save_diffexp_data.mlx| livescript to see how 
 % the initial dataset was preprocessed.
@@ -34,6 +24,11 @@ model.c(string(model.rxns) == 'biomass_human') = 1;
 
 % NFS
 load ~/Turbo/scampit/Analysis/EMT/scRNASeq_ihuman_diffexp.mat
+% Set the objective function to maximize biomass
+
+% Set up objective function
+model.c = zeros(size(model.c));
+model.c(string(model.rxns) == 'biomass_human') = 1;
 %% 3. Set flux balance analysis hyperparameters
 % Assign default values of parameters for the iMAT algorithm.
 
@@ -46,16 +41,18 @@ pfba = true;
 %% 4. Compute COBRA data
 % This code block fits the metabolic reconstruction with differentially expressed 
 % genes using the iMAT algorithm (|constrain_flux_regulation.m|).
-% Setting up parallel computations
+% A. Setting up parallel computations
 % We can set the number of CPUs to use for our calculations.
 
 %workers = 4;
 parpool("local", workers);
-% iHUMAN metabolic reconstruction
+% B. Initiate data structures
 % We'll use the iHUMAN metabolic reconstruction to perform flux balance analysis 
 % and knockouts.
-% 
-% This is the save file path.
+
+scRxnFx  = zeros(length(time_course), length(model.rxns));
+scGeneKO = zeros(length(time_course), length(model.genes));
+%scRxnKO  = zeros(length(time_course), length(model.rxns));
 
 % ACLX
 %filename = "~/Analysis/EMT/scRNASeq_ihuman_profiles.mat";
@@ -67,17 +64,13 @@ filename = "~/Turbo/scampit/Analysis/EMT/scRNASeq_ihuman_profiles.mat";
 %filename = "D:/Analysis/EMT/scRNASeq_ihuman_profiles.mat";
 
 % Save it
-save(filename, 'scRxnKO', 'scRxnFx', 'scGeneKO');
-% Single-cell simulations
+save(filename, 'scRxnFx', 'scGeneKO');
+% C. Perform Single-cell simulations
 % Now let's perform some knockouts. I will perform gene knockouts to save time.
 
-scRxnFx  = zeros(length(time_course), length(model.rxns));
-scGeneKO = zeros(length(time_course), length(model.genes));
-scRxnKO  = zeros(length(time_course), length(model.rxns));
-
-parfor j = 1:size(zdata, 2)
+parfor j = 1:size(ensembl_zdata, 2)
     
-    cell_data = zdata(:, j);
+    cell_data = ensembl_zdata(:, j);
     up_idx    = cell_data > 0;
     down_idx  = cell_data < 0;
         
@@ -96,14 +89,18 @@ parfor j = 1:size(zdata, 2)
                                                        isgenes, ...
                                                        pfba);
     
-    [geneKO, rxnKO] = knockOut(cell_mdl, 'All');
+    [geneKO, ~] = knockOut(cell_mdl, 'GeneKO');
     
     % Save data
-    scRxnKO(:, j)  = rxnKO;
-    scGeneKO(:, j) = geneKO
-    scRxnFx(:, j)  = soln;
+    scRxnKO(j, :)  = rxnKO;
+    scGeneKO(j, :) = geneKO
+    scRxnFx(j, :)  = soln;
     
     save(filename, ...
-         "scRxnKO", "scGeneKO", "scRxnFx", "j", ...
+         "scGeneKO", "scRxnFx", "j", ...
          "-append"); 
 end
+%% Summary
+% This notebook goes through bulk and single cell flux balance analysis. To 
+% get differentially active or differentially sensitive metabolic reactions, you 
+% can check out the |scDRSA.mlx| file.
