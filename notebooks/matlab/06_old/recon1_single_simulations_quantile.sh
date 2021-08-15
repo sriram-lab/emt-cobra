129981700f57c584792a524dad6266f647d2583a
Scott Campit <scampit@umich.edu>
2021-08-14 19:16:02 -0700

    Update recon1_single_simulations_quantile.sh

--- notebooks/matlab/06_old/recon1_single_simulations_quantile.sh
+++ notebooks/matlab/06_old/recon1_single_simulations_quantile.sh
@@ -3,15 +3,15 @@
 #SBATCH --mail-user=scampit@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --licenses=gurobi@slurmdb:8
-#SBATCH --output=./recon1_rho1_output.log
-#SBATCH --error=./recon1_rho1_error.err
+#SBATCH --output=./recon1_quantile_output.log
+#SBATCH --error=./recon1_quantile_error.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --mem-per-cpu=4g
-#SBATCH --time=03-00:00:00
+#SBATCH --time=01-00:00:00
 #SBATCH --account=lsa1
 #SBATCH --partition=standard
 
 module load matlab/R2018b
-module load gurobi
+module load gurobi/9.1.1
 matlab -nodisplay -r "run('/home/scampit/Turbo/scampit/Software/emt-cobra/notebooks/matlab/06_old/quantile_cobra_simulations.m'); exit"
